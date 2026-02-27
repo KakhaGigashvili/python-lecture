038a40bd88987cfba1aba84afe607054e5598366
kakha gigashvili <151166994+KakhaGigashvili@users.noreply.github.com>
2026-02-27 17:07:20 +0000

    piraty

--- calculator.py
+++
@@ -1,6 +0,0 @@
-x = float(input("What`s x? "))
-y = float(input("what`s y? "))
-
-z = x / y
-
-print(f"{x} + {y} = {z:.2f}")
---
+++ conditionals/compare.py
@@ -0,0 +1,9 @@
+x = int(input("What's X? "))
+y = int(input("What's Y? "))
+
+if x < y:
+    print("x is less than y")
+elif y > x:
+    print("y is greater than x")
+else:
+    print("x is equal y")
